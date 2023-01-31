d3164f4ff767dedaa618e3cad7766f6c4e525f8d
Zevi Cohen <zevihcohen@gmail.com>
2023-01-31 13:35:21 -0500

    got rid of bad code

--- test.py
+++ test.py
@@ -6,8 +6,8 @@ pygame.display.set_caption("This is pygame")
 
 x=50
 y=50
-width=50
-height=50
+width=40
+height=60
 vel=20
 
 run=True
@@ -17,23 +17,14 @@ while run:
         if event.type == pygame.QUIT:
             run=False
     keys= pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] and x>=20:
+    if keys[pygame.K_LEFT] and x>=0:
         x-=vel
-    elif keys[pygame.K_LEFT] and x<20:
-        x=0
-    if keys[pygame.K_RIGHT] and x<=550:
+    if keys[pygame.K_RIGHT] and x<=500:
         x+= vel
-    elif keys[pygame.K_RIGHT] and x>550:
-        x=550
-    if keys[pygame.K_UP]and y>=20:
+    if keys[pygame.K_UP]and y>=0:
         y-= vel
-    elif keys[pygame.K_UP]and y<20:
-        y=0
-    if keys[pygame.K_DOWN]and y<=550:
+    if keys[pygame.K_DOWN]and y<=500:
         y+= vel
-    elif keys[pygame.K_DOWN]and y>580:
-        y=550
-    
     win.fill((0,0,0))
     pygame.draw.rect(win, (255, 0 , 0), (x, y, width, height))
     pygame.display.update()
