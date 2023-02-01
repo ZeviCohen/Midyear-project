60d0e0ab53975df1fc0ea857e3c8dbcaa09e708e
swirlingvortex <goktugberatbas@gmail.com>
2023-02-01 09:32:29 -0500

    made classes

--- test.py
+++ test.py
@@ -1,25 +1,90 @@
-import pygame
+# import pygame
 
+# win = pygame.display.set_mode((600, 600))
+# pygame.display.set_caption("This is pygame")
+
+# # <<<<<<< HEAD
+# x=0
+# y=550
+# width=70
+# height=20
+# vel=5
+# x2 = 530
+# y2 = 550
+# cx = 300
+# cy = 300
+# cyvel = 5
+# # =======
+# # x=41
+# # y=41
+# # width=40
+# # height=60
+# # vel=20
+# # >>>>>>> 7252062a6370f7eca53081d683fa56010466519d
+
+# run=True
+# while run:
+#     pygame.time.delay(100)
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             run=False
+#     # keys= pygame.key.get_pressed()
+#     # if keys[pygame.K_LEFT] and x>vel:
+#     #     x-=vel
+#     # if keys[pygame.K_RIGHT] and x < 600 - width:
+#     #     x+= vel
+#     # if keys[pygame.K_UP]and y>=0:
+#     #     y-= vel
+#     # if keys[pygame.K_DOWN]and y < 600 - height:
+#     #     y+= vel
+#     # win.fill((0,0,0))
+#     # pygame.draw.rect(win, (255, 0 , 0), (x, y, width, height))
+#     # pygame.display.update()
+#     win.fill((0,0,0))
+#     pygame.draw.rect(win,(255,0,0), (x,y,width,height))
+#     pygame.draw.rect(win,(255,0,0),(x2,y2,width,height))
+#     pygame.draw.circle(win,(255,156,32),(cx,cy), (10))
+#     pygame.display.update()
+#     x += vel
+#     x2 -= vel
+#     cy += cyvel
+    
+#     if x > 530:
+#         vel *= -1
+#     if x < 0:
+#         vel *= -1
+#     keys = pygame.key.get_pressed()
+#     if keys[pygame.K_LEFT]:
+#         cx-=5
+#     if keys[pygame.K_RIGHT]:
+#         cx += 5
+#     if keys[pygame.K_UP]:
+#         cy-= 5
+#         cy-=5
+#         cy-=5
+#     if keys[pygame.K_DOWN]:
+#         cy += 5
+
+# pygame.quit()
+class Player(object):
+    def __init__(self):
+        self.playerx = 200
+        self.playery = 200
+        self.playerradius = 10
+        self.playeryvel = 5
+class Platform(object):
+    def __init__(self):
+        self.plat1x = 70
+        self.plat2x = 530
+        self.plat1y = 550
+        self.plat2y = 550
+        self.plat1width = 70
+        self.plat1height = 20
+        self.plat2width = 70
+        self.plat2height = 20
+import pygame
 win = pygame.display.set_mode((600, 600))
 pygame.display.set_caption("This is pygame")
-
-# <<<<<<< HEAD
-x=0
-y=550
-width=70
-height=20
-vel=5
-x2 = 530
-y2 = 550
-cx = 300
-cy = 300
-# =======
-# x=41
-# y=41
-# width=40
-# height=60
-# vel=20
-# >>>>>>> 7252062a6370f7eca53081d683fa56010466519d
 
 run=True
 while run:
@@ -27,38 +92,5 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run=False
-    # keys= pygame.key.get_pressed()
-    # if keys[pygame.K_LEFT] and x>vel:
-    #     x-=vel
-    # if keys[pygame.K_RIGHT] and x < 600 - width:
-    #     x+= vel
-    # if keys[pygame.K_UP]and y>=0:
-    #     y-= vel
-    # if keys[pygame.K_DOWN]and y < 600 - height:
-    #     y+= vel
-    # win.fill((0,0,0))
-    # pygame.draw.rect(win, (255, 0 , 0), (x, y, width, height))
-    # pygame.display.update()
-    win.fill((0,0,0))
-    pygame.draw.rect(win,(255,0,0), (x,y,width,height))
-    pygame.draw.rect(win,(255,0,0),(x2,y2,width,height))
-    pygame.draw.circle(win,(255,156,32),(cx,cy), (10))
-    pygame.display.update()
-    x += vel
-    x2 -= vel
-    cy += 5
-    if x > 530:
-        vel *= -1
-    if x < 0:
-        vel *= -1
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        cx-=5
-    if keys[pygame.K_RIGHT]:
-        cx += 5
-    if keys[pygame.K_UP]:
-        cy-= 15
-    if keys[pygame.K_DOWN]:
-        cy += 5
-
+    
 pygame.quit()
