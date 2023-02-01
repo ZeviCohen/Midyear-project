488cf8fad4f773f244ab3baac343fa078b2da8be
swirlingvortex <goktugberatbas@gmail.com>
2023-02-01 11:09:33 -0500

    L bozo u guys suck

--- gun_mayhem.py
+++ gun_mayhem.py
@@ -16,15 +16,15 @@ class Player(object):
         self.x += self.xvel
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
-            self.x-= 15
+            self.x-= 5
         if keys[pygame.K_RIGHT]:
-            self.x += 15
+            self.x += 5
         if keys[pygame.K_UP]:
             if (player1.x >= platform2.rect.x and player1.x <= platform2.rect.x + 70 and player1.y <= platform2.rect.y - 10 and player1.y >= platform2.rect.y - 12) or (player1.x >= platform2.rect.x and player1.x <= platform2.rect.x + 70 and player1.y <= platform2.rect.y - 10 and player1.y >= platform2.rect.y - 12):
-                self.y -= 45
+                self.y -= 15
         if keys[pygame.K_DOWN]:
             if (player1.x >= platform1.rect.x and player1.x <= platform1.rect.x + 70 and player1.y <= platform1.rect.y - 10 and player1.y >= platform1.rect.y - 12) or (player1.x >= platform2.rect.x and player1.x <= platform2.rect.x + 70 and player1.y <= platform2.rect.y - 10 and player1.y >= platform2.rect.y - 12) :
-                self.y += 15
+                self.y += 5
 class Platform(object):
     def __init__(self,x,y,width,height,vel):
         self.rect = pygame.Rect(x,y,width,height)
--- test.py
+++ test.py
@@ -66,19 +66,34 @@ import pygame
 #         cy += 5
 
 # pygame.quit()
+white = (255, 255, 255)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+black = (0, 0, 0)
 class Player(object):
-    def __init__(self,x,y,radius,vel):
+    def __init__(self,x,y,radius,yvel,xvel):
         self.x = x
         self.y = y
         self.radius = radius
-        self.vel = vel
+        self.yvel = yvel
+        self.xvel = xvel
+    def move(self):
+        self.y += self.yvel
+        self.x += self.xvel
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.x-= 5
+        if keys[pygame.K_RIGHT]:
+            self.x += 5
+        if keys[pygame.K_UP]:
+            if player1.x >= platform1.rect.x and player1.x <= platform1.rect.x + 70 and player1.y <= platform1.rect.y - 10 and player1.y >= platform1.rect.y - 12:
+                self.y -= 15
+        if keys[pygame.K_DOWN]:
+            self.y += 5
 class Platform(object):
     def __init__(self,x,y,width,height,vel):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.rect = pygame.Rect(self.x,self.y,self.width,self.height)
+        self.rect = pygame.Rect(x,y,width,height)
         self.vel = vel
     def moves(self):
         self.rect.x += self.vel
@@ -89,7 +104,7 @@ class Platform(object):
         pygame.display.update()
 platform1 = Platform(0,550,70,20, 5)
 platform2 = Platform(530,550,70,20, -5)
-player1 = Player(200,200,10,5)
+player1 = Player(200,200,10,5,0)
 win = pygame.display.set_mode((600, 600))
 pygame.display.set_caption("This is pygame")
 run=True
@@ -99,10 +114,19 @@ while run:
         if event.type == pygame.QUIT:
             run=False
     win.fill((0,0,0))
-    pygame.draw.rect(win,(255,0,0),platform1.rect)
-    pygame.draw.rect(win,(255,0,0),platform2.rect)
+    pygame.draw.rect(win,red,platform1.rect)
+    pygame.draw.rect(win,red,platform2.rect)
+    pygame.draw.circle(win,green,(player1.x,player1.y),player1.radius)
     platform1.moves()
     platform2.moves()
+    player1.move()
     pygame.display.update()
+    if player1.x >= platform1.rect.x and player1.x <= platform1.rect.x + 70 and player1.y <= platform1.rect.y - 10 and player1.y >= platform1.rect.y - 12:
+        player1.yvel = 0
+        player1.xvel = platform1.vel
+    elif player1.x >= platform2.rect.x and player1.x <= platform2.rect.x + 70 and player1.y <= platform2.rect.y - 10 and player1.y >= platform1.rect.y - 12:
+    else:
+        player1.yvel = 5
+        player1.xvel = 0
 
 pygame.quit()
