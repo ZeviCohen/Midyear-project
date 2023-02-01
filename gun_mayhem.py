90a9a5d29684474f3b384f3f4123c2032e2f9eec
Zevi Cohen <zevihcohen@gmail.com>
2023-02-01 13:20:14 -0500

    gabeitch

--- gun_mayhem.py
+++ gun_mayhem.py
@@ -14,9 +14,9 @@ class Player(object):
         self.square.x += self.xvel
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
-            self.x-= 5
+            self.square.x-= 5
         if keys[pygame.K_RIGHT]:
-            self.x += 5
+            self.square.x += 5
         if keys[pygame.K_DOWN]:
             if (player1.square.x >= platform1.rect.x and player1.square.x <= platform1.rect.x + 70 and player1.square.y <= platform1.rect.y - 10 and player1.square.y >= platform1.rect.y - 12) or (player1.square.x >= platform2.rect.x and player1.square.x <= platform2.rect.x + 70 and player1.square.y <= platform2.rect.y - 10 and player1.square.y >= platform2.rect.y - 12) :
                 self.square.y += 5
