a3a4e20e4c9910c8646491ee5a986ceb97f09c44
Zevi Cohen <zevihcohen@gmail.com>
2023-02-13 13:29:39 -0500

    upgrade class made

--- gun_mayhem.py
+++ gun_mayhem.py
@@ -3,7 +3,9 @@ import pygame, math
 #Color Palette
 color_dict = {"white":(255, 255, 255),"red":(255, 0, 0), "green":(0, 255, 0), "blue":(0, 0, 255), "black":(0, 0, 0)}
 pygame.init()
+gun_list = []
 bullet_list = []
+upgrade_list = []
 scrn = pygame.display.set_mode((600, 600))
 player1image = pygame.image.load("Images/Meowth-Pokemon-PNG-Transparent-Image.png").convert()
 def update_window():
@@ -134,7 +136,7 @@ class Player(object):
                 self.square.x += 10
                 self.lastrecorded = 'RIGHT'
             if keys[pygame.K_s]:
-                if (self.square.x >= platform1.rect.x and self.square.x <= platform1.rect.x + platform1.rect.width and self.square.y <= platform1.rect.y and self.square.y >= platform1.rect.y - 20) or (self.square.x >= platform2.rect.x and self.square.x <= platform2.rect.x + platform2.rect.width and self.square.y <= platform2.rect.y and self.square.y >= platform2.rect.y - 20) or (self.square.x >= platform3.rect.x and self.square.x <= platform3.rect.x + platform3.rect.width and self.square.y <= platform3.rect.y and self.square.y >= platform3.rect.y - 20):
+                if self.touching_platform==True:
                     self.square.y += 5
                     self.jumpcount = 2
             #Jumps
@@ -201,6 +203,28 @@ class Player(object):
         else:
             self.xvel = 0
             self.yvel = 15
+    def upgraded(self, upgrade):
+        if self.upgrade.power == 1:
+            pass
+        if self.upgrade.power == 2:
+            pass
+        if self.upgrade.power == 3:
+            pass
+        if self.upgrade.power == 4:
+            pass
+        if self.upgrade.power == 5:
+            pass
+        if self.upgrade.power == 6:
+            pass
+        if self.upgrade.power == 7:
+            pass
+        if self.upgrade.power == 8:
+            pass
+        if self.upgrade.power == 9:
+            pass
+        if self.upgrade.power == 10:
+            pass
+
 
 class Platform(object):
     def __init__(self,x,y,width,height,vel):
@@ -214,7 +238,36 @@ class Platform(object):
             self.vel *= -1
         pygame.display.update()
 
-
+class Upgrade(object):
+    def __init__(self, powerId = 0):
+        self.x = math.random.randint(20, 580)
+        self.y = 50
+        self.height = 15
+        self.width = 15
+        self.yvel = 10
+        #If powerId is 0, the upgrade is a powerup. Else, it is a gun.
+        self.powerId = powerId
+        if self.powerId == 0:
+            self.choose_powerup()
+        else:
+            self.decide_gun()
+    def move(self):
+        self.y += self.yvel
+    def check_for_platform(self, platform1, platform2, platform3):
+        #Detecs if player is on platform or not(Platform Collision)
+        if self.x >= platform1.rect.x and self.x <= platform1.rect.x + platform1.rect.width and self.y + self.height <= platform1.rect.y and self.y + self.height>= platform1.rect.y - 30:
+            self.yvel = 0
+            self.y = (platform1.rect.y - self.height)
+        elif self.x >= platform2.rect.x and self.x <= platform2.rect.x + platform2.rect.width and self.y + self.height<= platform2.rect.y and self.y + self.height >= platform2.rect.y - 30:
+            self.yvel = 0
+            self.y = (platform2.rect.y - self.height)
+        elif self.x >= platform3.rect.x and self.x <= platform3.rect.x + platform3.rect.width and self.y + self.height <= platform3.rect.y and self.y + self.height >= platform3.rect.y - 30:
+            self.yvel = 0
+            self.y = (platform3.rect.y - self.height)
+    def choose_powerup(self):
+        pass     
+    def decide_gun(self):
+        pass   
 #Creates all of the objects
 #Order goes as follows: x, y, width, height, vel
 platform1 = Platform(0,550,70,10, 5)
@@ -231,6 +284,9 @@ gun2 = Gun(player2, 10, 400, 50)
 win = pygame.display.set_mode((600, 600))
 pygame.display.set_caption("This is pygame")
 run = True
+
+#Code for upgrade timer
+upgrade_now = pygame.time.get_ticks()
 
 #Main
 pygame.display.flip()
@@ -266,6 +322,7 @@ while run:
         gun1.shoot()
     if keys[pygame.K_z]:
         gun2.shoot()
+    #Bullet code
     for bullet in bullet_list:
         if bullet.x > 600 or bullet.x < 0 or bullet.hit_enemy == True:
             bullet_list.remove(bullet)
@@ -277,5 +334,19 @@ while run:
                 pygame.draw.rect(win,color_dict['red'],(bullet.x, bullet.y,bullet.width,bullet.height))
                 bullet.check_collision(player1)
             bullet.move()
+    #Upgrade code
+    upgrade_last = pygame.time.get_ticks()
+    if upgrade_now - upgrade_last >= 500:
+        randchance = math.random.ranint(1,2)
+        if randchance == 1:
+            upgrade = Upgrade()
+            upgrade_list.append(upgrade)
+        upgrade_now = upgrade_last
+    for upgrade in upgrade_list:
+        upgrade.check_for_platform(platform1, platform2, platform3)
+        upgrade.move()
+        pygame.draw.circle(upgrade.x, upgrade.y, upgrade.width, upgrade.height)
+        if :
+            upgrade_list.remove(upgrade)
     pygame.display.update()
 pygame.quit()
