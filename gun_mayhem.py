6e67a6ae034e1c99f656c02fd53bf485df1c8e32
Zevi Cohen <zevihcohen@gmail.com>
2023-02-21 02:40:56 -0500

    upgrades now show on screen, platforms added

--- gun_mayhem.py
+++ gun_mayhem.py
@@ -1,4 +1,4 @@
-import pygame, math
+import pygame, math, random
 # copy of previous platform detection code: (self.square.x >= platform1.rect.x and self.square.x <= platform1.rect.x + 70 and self.square.y <= platform1.rect.y - 15 and self.square.y >= platform1.rect.y - 18) or (self.square.x >= platform2.rect.x and self.square.x <= platform2.rect.x + 70 and self.square.y <= platform2.rect.y - 15 and self.square.y >= platform2.rect.y - 18) or (self.square.x >= platform3.rect.x and self.square.x <= platform3.rect.x + 300 and self.square.y <= platform3.rect.y - 15 and self.square.y >= platform3.rect.y - 18)
 #Color Palette
 color_dict = {"white":(255, 255, 255),"red":(255, 0, 0), "green":(0, 255, 0), "blue":(0, 0, 255), "black":(0, 0, 0)}
@@ -16,6 +16,9 @@ def update_window():
     pygame.draw.rect(win,color_dict["red"],platform1.rect)
     pygame.draw.rect(win,color_dict["red"],platform2.rect)
     pygame.draw.rect(win,color_dict['red'],platform3.rect)
+    pygame.draw.rect(win,color_dict["red"],platform4.rect)
+    pygame.draw.rect(win,color_dict["red"],platform5.rect)
+    pygame.draw.rect(win,color_dict['red'],platform6.rect)
     #Draws the players
     pygame.draw.rect(win,color_dict["green"],(player1.square))
     pygame.draw.rect(win,color_dict['red'],(player2.square))
@@ -75,15 +78,6 @@ class Bullet(object):
             enemy.ishit = True
             self.hit_enemy = True
             enemy.bullet = self
-    def shot(self, enemy):
-        if enemy.ishit:
-            if self.ykb > 0:
-                enemy.square.y -= self.ykb
-                self.ykb -= 1
-                enemy.square.x += 1/4 * (self.xkb * self.direction)
-            else:
-                enemy.ishit = False
-                enemy.yvel = 10
 #Our player class
 class Player(object):
     def __init__(self,x,y,width,height,yvel,xvel, mass, jvel, player_num, lives):
@@ -196,14 +190,14 @@ class Player(object):
 
     def check_for_platform(self, platform1, platform2, platform3):
         #Detecs if player is on platform or not(Platform Collision)
-        if self.square.x >= platform1.rect.x and self.square.x <= platform1.rect.x + platform1.rect.width and self.square.y <= platform1.rect.y and self.square.y >= platform1.rect.y - 30:
+        if self.square.x >= platform1.rect.x and self.square.x <= platform1.rect.x + platform1.rect.width and self.square.y + self.square.height <= platform1.rect.y and self.square.y + self.square.height >= platform1.rect.y - 30:
             self.xvel = platform1.vel
             self.jumpcount = 0
             self.yvel = 0
             if (self.isJump == True and self.jvel <= -6) or self.isJump == False:
                 self.square.y = (platform1.rect.y - self.square.height)
                 self.touching_platform = True
-        elif self.square.x >= platform2.rect.x and self.square.x <= platform2.rect.x + platform2.rect.width and self.square.y <= platform2.rect.y and self.square.y >= platform2.rect.y - 30:
+        elif self.square.x >= platform2.rect.x and self.square.x <= platform2.rect.x + platform2.rect.width and self.square.y + self.square.height <= platform2.rect.y and self.square.y + self.square.height >= platform2.rect.y - 30:
             self.xvel = platform2.vel
             self.jumpcount = 0
             self.yvel = 0
@@ -223,34 +217,33 @@ class Player(object):
             self.touching_platform = False
     def shot(self, bullet):
         if self.ishit:
-            if bullet.ykb >= -5:
-                if bullet.ykb >= 0:
-                    F = (self.mass ** -1) *(bullet.ykb**2)
-                elif bullet.ykb < 0:
-                    F = (self.mass**-1) * -1 * (bullet.ykb**2)
+            if bullet.ykb >= 0:
+                F = (self.mass ** -1) *(bullet.ykb**2)
                 self.square.y-= F
                 bullet.ykb -= 1
+            else:
+                self.yvel = 10
             self.square.x += (bullet.xkb * bullet.direction)
     def upgraded(self, upgrade):
-        if self.upgrade.power == 1:
+        if upgrade.powerId == 1:
             pass
-        if self.upgrade.power == 2:
+        if upgrade.powerId == 2:
             pass
-        if self.upgrade.power == 3:
+        if upgrade.powerId == 3:
             pass
-        if self.upgrade.power == 4:
+        if upgrade.powerId == 4:
             pass
-        if self.upgrade.power == 5:
+        if upgrade.powerId == 5:
             pass
-        if self.upgrade.power == 6:
+        if upgrade.powerId == 6:
             pass
-        if self.upgrade.power == 7:
+        if upgrade.powerId == 7:
             pass
-        if self.upgrade.power == 8:
+        if upgrade.powerId == 8:
             pass
-        if self.upgrade.power == 9:
+        if upgrade.powerId == 9:
             pass
-        if self.upgrade.power == 10:
+        if upgrade.powerId == 10:
             pass
 
 
@@ -264,27 +257,36 @@ class Platform(object):
             self.vel *= -1
         if self.rect.x < 0:
             self.vel *= -1
-        pygame.display.update()
 
 class Upgrade(object):
-    def __init__(self):
-        self.x = math.random.randint(20, 580)
-        self.y = 50
-        self.height = 15
+    def __init__(self, platform, gunbox_check):
         self.width = 15
+        if gunbox_check:
+            self.y = 500
+            self.x = random.randint(20, 580)
+        else:
+            self.y = platform.rect.y - platform.rect.height
+            num1 = platform.rect.x
+            num2 = (platform.rect.x + platform.rect.width)- self.width
+            self.x = random.randint(num1, num2)
+        self.height = 15
         self.yvel = 10
-        self.powerId = math.random.randint(1, 10)
+        self.powerId = random.randint(1, 10)
+        self.radius = 10
+        self.platform = platform
+        #True means getting bigger while false means getting smaller
+        self.radius_change_state = True
     def move(self):
         self.y += self.yvel
     def check_for_platform(self, platform1, platform2, platform3):
         #Detecs if player is on platform or not(Platform Collision)
-        if self.x >= platform1.rect.x and self.x <= platform1.rect.x + platform1.rect.width and self.y + self.height <= platform1.rect.y and self.y + self.height>= platform1.rect.y - 30:
+        if self.x >= platform1.rect.x and self.x <= platform1.rect.x + platform1.rect.width and self.y + self.height <= platform1.rect.y and self.y + self.height>= platform1.rect.y - 45:
             self.yvel = 0
             self.y = (platform1.rect.y - self.height)
-        elif self.x >= platform2.rect.x and self.x <= platform2.rect.x + platform2.rect.width and self.y + self.height<= platform2.rect.y and self.y + self.height >= platform2.rect.y - 30:
+        elif self.x >= platform2.rect.x and self.x <= platform2.rect.x + platform2.rect.width and self.y + self.height<= platform2.rect.y and self.y + self.height >= platform2.rect.y - 45:
             self.yvel = 0
             self.y = (platform2.rect.y - self.height)
-        elif self.x >= platform3.rect.x and self.x <= platform3.rect.x + platform3.rect.width and self.y + self.height <= platform3.rect.y and self.y + self.height >= platform3.rect.y - 30:
+        elif self.x >= platform3.rect.x and self.x <= platform3.rect.x + platform3.rect.width and self.y + self.height <= platform3.rect.y and self.y + self.height >= platform3.rect.y - 45:
             self.yvel = 0
             self.y = (platform3.rect.y - self.height)
     def choose_random_gun(self, player):
@@ -293,44 +295,60 @@ class Upgrade(object):
             gun.owner = player
             player.mainngun = player.gun
             player.gun = gun
+    def radius_change(self):
+        if self.radius_change_state:
+            self.radius += 0.5
+        else:
+            self.radius -= 0.5
+        if self.radius <= 9:
+            self.radius_change_state = True
+        if self.radius >= 11:
+            self.radius_change_state = False
+    def platform_move(self):
+        self.x += self.platform.vel
+
+        
 
 #Creates all of the objects
 #Order goes as follows: x, y, width, height, vel
 platform1 = Platform(0,550,70,10, 5)
 platform2 = Platform(530,550,70,10, -5)
-platform3 = Platform(150,450,300,10,0)
+platform3 = Platform(150,510,300,10,0)
+platform4 = Platform(20,470,100,10,0)
+platform5 = Platform(500,470,100,10,0)
+platform6 = Platform(150,430,300,10,0)
 #Order goes as follows: x,y,width,height,yvel,xvel, mass, jvel, player_num, lives
-player1 = Player(300,100,15,15,10,0,1,8, 1, 10)
-player2 = Player(300,100,15,15,10,0,1,8, 2, 10)
-#Order goes as follows: owner, ammo, cooldown, bullet_kb
-maingun1 = Gun(player1, 10, 10, 5, 0)
+player1 = Player(300,100,18,35,10,0,1,8, 1, 10)
+player2 = Player(300,100,18,35,10,0,1,8, 2, 10)
+#Order goes as follows: owner, ammo, cooldown, bullet_kb, gunid
+maingun1 = Gun(player1, 10, 400, 10, 0)
 player1.maingun = maingun1
 player1.gun = maingun1
-maingun2 = Gun(player2, 10, 10, 5, 0)
+maingun2 = Gun(player2, 10, 400, 10, 0)
 player2.maingun = maingun2
 player2.gun = maingun2
 
 #Gun List:
-gun_1 = Gun(None, 50, 10, 3, 1)#Sub machine gun
-gun_2 = Gun(None, 5, 100, 10, 1)#Sniper
-gun_3 = Gun(None, 5, 100, 10, 1)#Shotgun
-gun_4 = Gun(None, 30, 35, 3, 1)#Assault rifle
-gun_5 = Gun(None, 50, 10, 3, 1)#Light machine gun
+gun_1 = Gun(None, 50, 200, 3, 1)#Sub machine gun
+gun_2 = Gun(None, 5, 500, 10, 1)#Sniper
+gun_3 = Gun(None, 5, 500, 10, 1)#Shotgun
+gun_4 = Gun(None, 30, 250, 3, 1)#Assault rifle
+gun_5 = Gun(None, 50, 200, 3, 1)#Light machine gun
 #Special
-gun_6 = Gun(None, 100, 0, 3, 1)#Minigun
-gun_7 = Gun(None, 3, 150, 4, 1)#Dematerializer
+gun_6 = Gun(None, 100, 100, 3, 1)#Minigun
+gun_7 = Gun(None, 3, 750, 4, 1)#Dematerializer
 #Gun_list stores all the special guns that arrive in lootboxes
 #Sets up the window
 win = pygame.display.set_mode((600, 600))
 pygame.display.set_caption("This is pygame")
 pygame.display.flip()
-player1image = pygame.transform.scale(player1image, DEFAULT_IMAGE_SIZE)
+player1image = pygame.transform.scale(player1image, (player1.square.width, player1.square.height))
 run = True
 
 #Code for upgrade timer
-upgrade_now = pygame.time.get_ticks()
+upgrade_last = pygame.time.get_ticks()
 #Code for gunbox timer
-gunbox_now = pygame.time.get_ticks()
+gunbox_last = pygame.time.get_ticks()
 
 #Main
 pygame.display.flip()
@@ -346,7 +364,6 @@ while run:
     scrn.blit(player1image, (player1.square.x, player1.square.y))
     platform1.moves()
     platform2.moves()
-    platform3.moves()
     #Actions of both players
     #Moves and Jumps
     player1.move()
@@ -361,6 +378,8 @@ while run:
     #Check for platform collision
     player1.check_for_platform(platform1, platform2, platform3)
     player2.check_for_platform(platform1, platform2, platform3)
+    player1.check_for_platform(platform4, platform5, platform6)
+    player2.check_for_platform(platform4, platform5, platform6)
     #Shoots
     if keys[pygame.K_SPACE]:
         player1.gun.shoot()
@@ -381,31 +400,45 @@ while run:
     player1.shot(player1.bullet)
     player2.shot(player2.bullet)
     #Upgrade code
-    upgrade_last = pygame.time.get_ticks()
-    if upgrade_now - upgrade_last >= 500:
-        upgrade_now = upgrade_last
-        randchance = math.random.ranint(1,2)
-        if randchance == 1:
-            upgrade = Upgrade()
-            upgrade_list.append(upgrade)
+    upgrade_now = pygame.time.get_ticks()
+    if upgrade_now - upgrade_last >= 10000:
+        upgrade_last = upgrade_now
+        if len(upgrade_list) < 2:
+            randchance = random.randint(1,2)
+            if randchance == 1:
+                upgrade_platform = random.randint(1, 6)
+                if upgrade_platform == 1:
+                    upgrade = Upgrade(platform1, False)
+                if upgrade_platform == 2:
+                    upgrade = Upgrade(platform2, False)
+                if upgrade_platform == 3:
+                    upgrade = Upgrade(platform3, False)
+                if upgrade_platform == 4:
+                    upgrade = Upgrade(platform4, False)
+                if upgrade_platform == 5:
+                    upgrade = Upgrade(platform5, False)
+                if upgrade_platform == 6:
+                    upgrade = Upgrade(platform6, False)
+                upgrade_list.append(upgrade)
     for upgrade in upgrade_list:
-        upgrade.check_for_platform(platform1, platform2, platform3)
-        upgrade.move()
-        pygame.draw.circle(win, color_dict["blue"], (upgrade.x, upgrade.y, upgrade.width, upgrade.height))
+        upgrade.radius_change()
+        upgrade.platform_move()
+        pygame.draw.circle(win, color_dict["blue"], (upgrade.x, upgrade.y), upgrade.radius)
         if (upgrade.x<=player1.square.x+player1.square.width) and (upgrade.x + upgrade.width >= player1.square.x) and (upgrade.y <= player1.square.y+ player1.square.height) and (upgrade.y + upgrade.width >= player1.square.y):
-            player1.upgraded()
+            player1.upgraded(upgrade)
             upgrade_list.remove(upgrade)
         elif (upgrade.x<=player2.square.x+player2.square.width) and (upgrade.x + upgrade.width >= player2.square.x) and (upgrade.y <= player2.square.y+ player2.square.height) and (upgrade.y + upgrade.width >= player2.square.y):
-            player2.upgraded()
+            player2.upgraded(upgrade)
             upgrade_list.remove(upgrade)
     #Gunbox code
-    gunbox_last = pygame.time.get_ticks()
-    if gunbox_now-gunbox_last >= 300:
-        gunbox_now = gunbox_last
-        gunbox = Upgrade()
+    gunbox_now = pygame.time.get_ticks()
+    if gunbox_now - gunbox_last >= 300:
+        gunbox_last = gunbox_now
+        gunbox = Upgrade(None, True)
         gunbox_list.append(gunbox)
     for gunbox in gun_list:
         gunbox.check_for_platform(platform1, platform2, platform3)
+        gunbox.check_for_platform(platform4, platform5, platform6)
         gunbox.move()
         pygame.draw.rect(win, color_dict["blue"], (gunbox.x, gunbox.y, gunbox.width, gunbox.height))
         if (gunbox.x<=player1.square.x+player1.square.width) and (gunbox.x + gunbox.width >= player1.square.x) and (gunbox.y <= player1.square.y+ player1.square.height) and (gunbox.y + gunbox.width >= player1.square.y):
